3ea39ea09a497c93a3845350a099bddef4803149
Luc J <kelu124@gmail.com>
2025-10-24 15:23:24 +0200

    autoupdate

--- fwk_colors.py
+++
@@ -1,5 +0,0 @@
-summary = ["Write a five line executive summary of the following content. There should be no bullet point, only text, and it needs to be concise and precise."]
-
-OOI = ["Think of 'output' as defined in the 'logical framework' (or logframe) used in monitoring and evaluation frameworks. Is the intended output highly satisfactory, satisfatory but improvements needed or not satisfactory? ",
-"Think of 'outcome' as defined in the 'logical framework' (or logframe) used in monitoring and evaluation frameworks. Is the intended outcome highly satisfactory, satisfatory but improvements needed or not satisfactory?",
-"Think of 'impact' as defined in the 'logical framework' (or logframe) used in monitoring and evaluation frameworks. Is the intended impact highly satisfactory, satisfatory but improvements needed, not satisfactory or irrelevant?"]
